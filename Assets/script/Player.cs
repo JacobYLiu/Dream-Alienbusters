13ac9cd67bbee1ebe44240bbbfe308466bc17ace
kulaczx <yli607@ucr.edu>
2021-07-08 13:28:12 -0700

    improve movement, add camera movement

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 0.1f;//need to adjust base on map size
+    public float speed = 25f;//need to adjust base on map size
     public int hp = 100;
     public bool isDead = false;
     public CharacterController myController;
+    public Transform camaraHead;//camera control
+
+    private float cameraVerticalRotation;
+    public float mouseSensitivity = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +21,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        player_movement();
+        player_view();
+    }
+
+    void player_view()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;//get mouse x input
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;//get mouse y input
+
+        cameraVerticalRotation -= mouseY;
+        transform.Rotate(Vector3.up * mouseX);// set camara and character rotate with mouse x input
+        camaraHead.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+
+    }
+
+    void player_movement()
+    {
         float x = Input.GetAxis("Horizontal");//get x axis
         float z = Input.GetAxis("Vertical");//get z axis
 
         Vector3 move = x * transform.right + z * transform.forward;//set move vector
+        move = move * speed * Time.deltaTime;//set movement base on time
+
+
 
         myController.Move(move);//set controller
     }
